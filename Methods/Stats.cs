5d767932f72cc633551bfc53fe20d31fe0c36dc9
shahrzadazari <shahrzad2780@example.com>
2020-09-07 23:28:01 +0430

    app UI

--- Methods/Stats.cs
+++ Methods/Stats.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -11,27 +12,44 @@ namespace BlazorApp.Methods
         public Stats(){}
         public TotalReportModel[] TotalReport {get;set;} = {};
         public NewsResponseModel NewsResponse {get;set;} = null;
+        public List<ArticleModel> AllArticles {get;set;} = new List<ArticleModel>();
         public ChartDataModel[] ChartDatas {get;set;} = {};
         public HttpClient Http = new HttpClient();
 
         public async Task GetArticles()
         {
-            HttpResponseMessage response = null ;
-            try
-            {
-                var uri = @"https://newsapi.org/v2/everything?apiKey=4a892542050f40d6a39c3b76785e8240&domains=cnn.com&q=covid vaccine";
-                var httpRequest = new HttpRequestMessage
+            String date = DateTime.Today.ToString("yyyy-MM-dd");
+            do{
+                HttpResponseMessage response = null ;
+                try
                 {
-                    Method = HttpMethod.Get,
-                    RequestUri = new Uri(uri),
-                };
-                response = await Http.SendAsync(httpRequest);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            this.NewsResponse = response != null ? await response.Content.ReadFromJsonAsync<NewsResponseModel>() : null;
+                    var uri = @"https://newsapi.org/v2/everything?apiKey=4a892542050f40d6a39c3b76785e8240&domains=cnn.com&q=covid vaccine&sortBy=publishedAt&to="+date;
+                    var httpRequest = new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Get,
+                        RequestUri = new Uri(uri),
+                    };
+                    response = await Http.SendAsync(httpRequest);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                if(response != null)
+                {
+                    this.NewsResponse = await response.Content.ReadFromJsonAsync<NewsResponseModel>();
+
+                    foreach(ArticleModel ar in this.NewsResponse.Articles)
+                        if(! AllArticles.Contains(ar))
+                            this.AllArticles.Add(ar);
+                    
+                    if(this.NewsResponse.Articles.Length > 0)
+                        date = this.NewsResponse.Articles[this.NewsResponse.Articles.Length -1].PublishedAt.ToString("yyyy-MM-dd");
+                    else
+                        break;
+                }
+
+            }while(this.NewsResponse.Articles.Length == 20);
         }
 
         public async Task GetStatistics()
--- Models/NewsResponseModel.cs
+++ Models/NewsResponseModel.cs
@@ -1,3 +1,4 @@
+
 namespace BlazorApp.Models
 {
     public class NewsResponseModel
