72f15ce06ba5adc20fac9e3e05c6ae5e423bd9ad
shahrzadazari <shahrzad2780@example.com>
2020-09-10 22:14:16 +0430

    nothing much

--- Methods/ArticleM.cs
+++ Methods/ArticleM.cs
@@ -16,7 +16,6 @@ namespace BlazorApp.Methods
         }
         public NewsResponseModel NewsResponse {get;set;} = new NewsResponseModel();
         public List<ArticleModel> AllArticles {get;set;} = new List<ArticleModel>();
-        public ArticleModel[] AllTheArticles {get;set;} = {};
         public async Task GetArticles()
         {
             String date = DateTime.Today.ToString("yyyy-MM-dd");
@@ -51,7 +50,6 @@ namespace BlazorApp.Methods
                 }
 
             }while(this.NewsResponse.Articles.Length == 20);
-            AllTheArticles = AllArticles.ToArray();
         }
     
     }
